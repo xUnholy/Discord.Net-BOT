1eefa9b621edbe1b7f4ef3431e05527509b5d8a4
Michael <Michael@USER-PC>
2016-09-01 23:32:28 +1000

    Implement Greeting
    Clean Code

--- Discord/BotConnect.cs
+++ Discord/BotConnect.cs
@@ -11,7 +11,7 @@ namespace Discord
 {
     class BotConnect
     {
-        public string Token = "MjIwNTAzODU2MjA1OTIyMzA0.Cqlsxw.5IZsTr5VWz0H7VC4ihhJQnJ72oQ";
+        public string Token = "<Token>";
 
         public List<Logs> MyErrorLogs = new List<Logs>();
         public List<Logs> MyRequestLogs = new List<Logs>();
@@ -23,7 +23,7 @@ namespace Discord
             _client = new DiscordClient(x =>
             {
                 x.AppName = "Ethereal";
-                x.AppUrl = "https://discordapp.com/oauth2/authorize?client_id=220396497009770497&scope=bot&permissions=0";
+                x.AppUrl = "https://discordapp.com/oauth2/authorize?client_id=220065941067792384&scope=bot&permissions=0";
                 x.LogLevel = LogSeverity.Info;
                 x.LogHandler = Log;
             });
@@ -35,39 +35,43 @@ namespace Discord
                 x.HelpMode = HelpMode.Private;
             });
 
-            _client.UsingPermissionLevels((u, c) => (int)GetPermissions(u, c));
-
             //Welcome new users to the server
-            _client.UserJoined += (sender, e) =>
-            {
-                e.User.SendMessage("Welcome to the Ethereal Bot Discord Server!");
-                e.User.SendMessage("Please check out #announcements and #readme");
-            };
+            _client.UserJoined += _client_UserJoined; 
 
             //Check if a user is staff, were offline, and now online, and greet them
-            _client.UserUpdated += (sender, e) =>
-            {
-                bool isStaff = false;
-                IEnumerable<Role> userRoles = e.After.Roles;
-                foreach (Role role in userRoles)
-                {
-                    if (role.Name.Contains("Lead Developer") || role.Name.Contains("Fearless Leader"))
-                    {
-                        isStaff = true;
-                    }
-                }
-                if (e.After.LastOnlineAt != e.Before.LastOnlineAt && isStaff)
-                {
-                    e.After.SendMessage($"Welcome {e.After.Name}!");
-                }                
-            };
-
+            _client.UserUpdated += _client_UserOnline;
+ 
             CreateCommands();
 
             _client.ExecuteAndWait(async () =>
             {
                 await _client.Connect(Token);
             });
+        }
+
+        private static async void _client_UserOnline(object sender, UserUpdatedEventArgs e)
+        {
+            var isStaff = false;
+            var userRoles = e.After.Roles;
+            foreach (var role in userRoles)
+            {
+                if (role.Name.Contains("Lead Developer") || role.Name.Contains("Fearless Leader"))
+                {
+                    isStaff = true;
+                }
+            }
+            if (e.Before.Status.Value == "offline" && e.After.Status.Value == "online" && isStaff)
+            {
+                await e.After.SendMessage($"Welcome {e.After.Name}!");
+            }
+        }
+
+        private static async void _client_UserJoined(object sender, UserEventArgs e)
+        {
+            await e.User.SendMessage($"Welcome {e.User.Mention} to the Ethereal Bot Discord Server!" +
+                                     "\n" +
+                                     "\nPlease check out #announcements and #readme");
+
         }
 
         public void CreateCommands()
@@ -130,9 +134,19 @@ namespace Discord
                 .AddCheck((c, u, ch) => ch.Id == 210512518076956673 || ch.Id == 220286522543308801)
                 .Do(async (e) =>
                 {
-                    foreach (var log in MyRequestLogs)
+                    if (e.Channel.Id == 210512518076956673)
                     {
-                        await e.User.SendMessage($"[{log.Date}] [*{log.User}*] [**Request**] -- {log.Logged}");
+                        foreach (var log in MyRequestLogs)
+                        {
+                            await e.Channel.SendMessage($"[{log.Date}] [*{log.User}*] [**Error**] -- {log.Logged}");
+                        }
+                    }
+                    else
+                    {
+                        foreach (var log in MyRequestLogs)
+                        {
+                            await e.User.SendMessage($"[{log.Date}] [*{log.User}*] [**Error**] -- {log.Logged}");
+                        }
                     }
                 });
 
@@ -150,7 +164,8 @@ namespace Discord
               .Do(async (e) =>
               {
                   var singleRole = e.Server.Roles.FirstOrDefault(x => x.Name == "Lead Developer");
-                  if (e.User.HasRole(singleRole))
+                  var secondRole = e.Server.Roles.FirstOrDefault(x => x.Name == "Fearless Leader");
+                  if (e.User.HasRole(singleRole) || e.User.HasRole(secondRole))
                   {
                       await e.User.SendMessage($"All error logs have been dumped");
 
@@ -179,36 +194,6 @@ namespace Discord
         public void Log(object sender, LogMessageEventArgs e)
         {
             Console.WriteLine($"[{e.Severity}] [{e.Source}] {e.Message}");
-        }
-
-        private static PermissionLevel GetPermissions(User u, Channel c)
-        {
-            if (u.Id == 220396497009770497) //Personal UserId
-                return PermissionLevel.BotOwner;
-
-            if (u.IsBot) // Customize this to your liking to ignore other stuff, like a list of known spammers.
-            {
-                return PermissionLevel.Ignored;
-            }
-
-            if (!c.IsPrivate)
-            {
-                if (u == c.Server.Owner)
-                    return PermissionLevel.ServerOwner;
-
-                var serverPerms = u.ServerPermissions;
-                if (serverPerms.ManageRoles || u.Roles.Select(x => x.Name.ToLower()).Contains("bot commander"))
-                    return PermissionLevel.ServerAdmin;
-                if (serverPerms.ManageMessages && serverPerms.KickMembers && serverPerms.BanMembers)
-                    return PermissionLevel.ServerModerator;
-
-                var channelPerms = u.GetPermissions(c);
-                if (channelPerms.ManagePermissions)
-                    return PermissionLevel.ChannelAdmin;
-                if (channelPerms.ManageMessages)
-                    return PermissionLevel.ChannelModerator;
-            }
-            return PermissionLevel.User;
         }
     }
 }
