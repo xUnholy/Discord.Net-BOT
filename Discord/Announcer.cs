ec33a4635db57ca4f9500fd11890b4ab08038672
ellm62 <ellm62@gmail.com>
2016-09-03 23:04:07 +0100

    Refactor
    Added settings helper class
    Added all current channel ids
    Implemented config file
    Implemented channel announcements
    Fixed Http exception
    Cleaned functions and command permissions

---
+++ Discord/Announcer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Timers;
+using System.Linq;
+using System.IO;
+
+namespace Discord
+{
+    class Announcer
+    {
+        private Timer msgTimer;
+        private Random rand = new Random();
+        int timerInterval = int.Parse(Settings.AnnouncerInterval);
+
+        public Announcer()
+        {
+            timerInterval = rand.Next(timerInterval - 300000, timerInterval + 300000);
+            msgTimer = new Timer(timerInterval);
+            msgTimer.Elapsed += new ElapsedEventHandler(msgAnnouncer);
+            msgTimer.Enabled = true;
+            Console.WriteLine($"Next announcement to {Settings.DefaultChan} in {((timerInterval / 1000) / 60)}minutes");
+        }
+
+        private async void msgAnnouncer(object s, ElapsedEventArgs e)
+        {
+            timerInterval = rand.Next(timerInterval - 300000, timerInterval + 300000);
+            msgTimer.Interval = timerInterval;
+
+            string[] _lines = File.ReadAllLines(Environment.CurrentDirectory + @"\Announcements.txt");
+            int _randLineNum = rand.Next(_lines.Length);
+            string _randLine = _lines[rand.Next(_randLineNum)];
+
+            if (_randLine != "")
+                await Connect._client.Servers.FirstOrDefault(x => x.Id == Settings.ServerID)?.TextChannels.FirstOrDefault(x => x.Name == Settings.DefaultChan).SendMessage("`Random Poké Fact #" + _randLineNum + "`\n" + _randLine);
+            else return;
+        }
+    }
+}
