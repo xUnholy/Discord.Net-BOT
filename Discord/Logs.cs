16734fb20cf44a714f568c602ba300386c6f4d41
ellm62 <ellm62@gmail.com>
2016-09-10 23:50:06 +0100

    Inital Broly Commit

--- Discord/Logs.cs
+++ Discord/Logs.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,30 @@ namespace Discord
     {
         public string Date { get; set; }
         public string User { get; set; }
+        public string LogType { get; set; }
         public string Logged { get; set; }
-             
+
+        public static void WriteToLogFile(string logPath, List<Logs> logName)
+        {
+            using (var file = new StreamWriter(logPath, true))
+            {
+                foreach (var log in logName)
+                {
+                    file.WriteLine($"[{log.Date}] [{log.LogType}] [{log.User}] -- {log.Logged}");
+                }
+                file.Close();
+            }
+        }
+
+        public static void AddToLog(string user, string msg, List<Logs> log, string type)
+        {
+            log.Add(new Logs()
+            {
+                Date        = $"{DateTime.Now.Date.ToString($"dd.MM.yy")}",
+                LogType     = $"{type}",
+                User        = $"{user}",
+                Logged      = $"{msg}"
+            });
+        }
     }
 }
